c7b5f152714d8fa119ae87ed765d2542a796dc9b
athongbai <46663583+athongbai@users.noreply.github.com>
2019-01-14 11:34:49 +0700

    Update webhooks.php

--- Desktop/line-bot-unlimit_friend-master/webhooks.php
+++ Desktop/line-bot-unlimit_friend-master/webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '*Channel access token*';
+$access_token = '+F9s6MU2rokX8HMa1nJ10E+lXnZsC8rNDhq3xZRkOXzOyXBbAvOslAV22xGLwBhR5qROBNfHUHazBNGWmPPS3WNymwihm+pW6F8fp+t2fk9HcoENwDYMN8foPadym3w2MYWsh3eUWZ+8bISJmcYfOAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -45,8 +45,8 @@ if (!is_null($events['events'])) {
 	}
 }
 require "vendor/autoload.php";
-$channelSecret = '*Channel secret*';
-$pushID = '*YourUserID*';
+$channelSecret = '061306855494354aa89f799f1d047228';
+$pushID = 'U31fbffd7c76cf39cfe8b92e1f5092163';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text2);
