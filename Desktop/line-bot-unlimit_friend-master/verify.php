a73d648d70323bc3c62b5357ab983698128f374c
athongbai <46663583+athongbai@users.noreply.github.com>
2019-01-14 11:33:18 +0700

    Update verify.php

--- Desktop/line-bot-unlimit_friend-master/verify.php
+++ Desktop/line-bot-unlimit_friend-master/verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '*Channel_access_token*';
+$access_token = '+F9s6MU2rokX8HMa1nJ10E+lXnZsC8rNDhq3xZRkOXzOyXBbAvOslAV22xGLwBhR5qROBNfHUHazBNGWmPPS3WNymwihm+pW6F8fp+t2fk9HcoENwDYMN8foPadym3w2MYWsh3eUWZ+8bISJmcYfOAdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
